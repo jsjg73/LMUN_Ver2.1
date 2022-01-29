ca6ee2328b7f6dae2c22130dfd6059f9c1d87006
jsjg73 <kjs930703@naver.com>
2022-01-30 00:17:58 +0900

    create boot project

---
+++ lmun/src/main/java/com/jsjg73/lmun/LmunApplication.java
@@ -0,0 +1,13 @@
+package com.jsjg73.lmun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmunApplication.class, args);
+	}
+
+}
---
+++ lmun/src/test/java/com/jsjg73/lmun/LmunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsjg73.lmun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
