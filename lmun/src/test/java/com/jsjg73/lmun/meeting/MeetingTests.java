af70ab537f341e5ec121a42b389bfdafb1649aa8
jsjg73 <kjs930703@naver.com>
2022-02-24 20:58:51 +0900

    수정/테스트 : 테스트 전처리 데이터 private 메소드화/ proposal

--- lmun/src/test/java/com/jsjg73/lmun/meeting/MeetingTests.java
+++ lmun/src/test/java/com/jsjg73/lmun/meeting/MeetingTests.java
@@ -298,34 +298,7 @@ public class MeetingTests {
     @Order(9)
     @DisplayName("제안 생성 실패(이미 제안된 장소)")
     public void registryProposalFail() throws Exception {
-        //TODO : 제안 생성 실패 테스트
-        LocationDto destination
-                = LocationDto.builder()
-                .id(26338954L)
-                .placeName("카카오프렌즈 코엑스점")
-                .addressName("서울 강남구 삼성동 159")
-                .roadAddressName("서울 강남구 영동대로 513")
-                .lat(37.51207412593136)
-                .lon(127.05902969025047)
-                .categoryGroupCode(Category.NULL)
-                .build();
-        OriginRequest origin1
-                = OriginRequest.builder()
-                .username("user1")
-                .lon(127.13243772760565)
-                .lat(37.44148514309502)
-                .build();
-        OriginRequest origin2
-                = OriginRequest.builder()
-                .username("user2")
-                .lon(127.1331694942593)
-                .lat(37.4463137562622)
-                .build();
-        ProposalRequest proposalRequest
-                = ProposalRequest.builder()
-                .destination(destination)
-                .origins(List.of(origin1, origin2))
-                .build();
+        ProposalRequest proposalRequest = proposalDataForTest();
 
         // register proposal
         mockMvc.perform(
@@ -365,6 +338,24 @@ public class MeetingTests {
     @Order(10)
     @DisplayName("제안 생성 성공")
     public void registryProposal() throws Exception {
+        ProposalRequest proposalRequest = proposalDataForTest();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/meeting/"+registeredMeeting.getId()+"/proposal")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(proposalRequest))
+                        .header("Authorization", "Bearer "+tokenForUser1)
+        ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(MeetingResource.class))
+                .andExpect(handler().methodName("registerProposal"))
+                .andExpect(jsonPath("$.meetingId").value(registeredMeeting.getId()))
+                .andExpect(jsonPath("$.locationId").value(26338954L));
+    }
+
+    private ProposalRequest proposalDataForTest() {
         LocationDto destination
                 = LocationDto.builder()
                 .id(26338954L)
@@ -387,24 +378,9 @@ public class MeetingTests {
                 .lon(127.1331694942593)
                 .lat(37.4463137562622)
                 .build();
-        ProposalRequest proposalRequest
-                = ProposalRequest.builder()
+        return ProposalRequest.builder()
                 .destination(destination)
                 .origins(List.of(origin1, origin2))
                 .build();
-
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                        .post("/meeting/"+registeredMeeting.getId()+"/proposal")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(proposalRequest))
-                        .header("Authorization", "Bearer "+tokenForUser1)
-        ).andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(handler().handlerType(MeetingResource.class))
-                .andExpect(handler().methodName("registerProposal"))
-                .andExpect(jsonPath("$.meetingId").value(registeredMeeting.getId()))
-                .andExpect(jsonPath("$.locationId").value(26338954L));
     }
 }
